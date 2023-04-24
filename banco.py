17ce92060d0f3127803dab7d7b030a8b4af39ff9
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-24 16:46:01 +0200

    creada la funcion ingresar y retirar

--- banco.py
+++ banco.py
@@ -2,3 +2,14 @@ import multiprocessing
 import time
 import random
 
+class Banco:
+
+    def ingreso(cuenta, cantidad):
+        cuenta.value += cantidad
+        print('Se han ingresado', cantidad, '€ en la cuenta')
+        time.sleep(random.random())
+
+    def retirar(cuenta, cantidad):
+        cuenta.value -= cantidad
+        print('Se ha retirado', cantidad, '€ de la cuenta')
+        time.sleep(random.random())

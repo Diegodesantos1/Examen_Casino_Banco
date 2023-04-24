f3c0ac4c6a763452f8f9c81212a4f6729b96a9ca
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-24 17:12:38 +0200

    creada la función jugar a un número

--- casino.py
+++ casino.py
@@ -1,3 +1,11 @@
 import multiprocessing
 import time
 import random
+class Casino:
+
+
+    def jugar_numero(cuenta, cantidad, numero):
+        cuenta.value -= cantidad
+        print('Se han apostado', cantidad,
+            '€ al número', numero)
+        time.sleep(random.random())

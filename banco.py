37085920e66e29b177d3a0bddd7e47a545390538
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-24 16:49:05 +0200

    creada la función principal

--- banco.py
+++ banco.py
@@ -4,7 +4,7 @@ import random
 
 class Banco:
 
-    def ingreso(cuenta, cantidad):
+    def ingresar(cuenta, cantidad):
         cuenta.value += cantidad
         print('Se han ingresado', cantidad, '€ en la cuenta')
         time.sleep(random.random())
@@ -13,3 +13,38 @@ class Banco:
         cuenta.value -= cantidad
         print('Se ha retirado', cantidad, '€ de la cuenta')
         time.sleep(random.random())
+
+    def main():
+        cuenta = multiprocessing.Value('i', 100)
+        procesos = []
+        for i in range(40):
+            p = multiprocessing.Process(target=Banco.ingresar, args=(cuenta, 100))
+            procesos.append(p)
+            p.start()
+        for i in range(20):
+            p = multiprocessing.Process(target=Banco.ingresar, args=(cuenta, 50))
+            procesos.append(p)
+            p.start()
+        for i in range(60):
+            p = multiprocessing.Process(target=Banco.ingresar, args=(cuenta, 20))
+            procesos.append(p)
+            p.start()
+        for i in range(40):
+            p = multiprocessing.Process(target=Banco.retirar, args=(cuenta, 100))
+            procesos.append(p)
+            p.start()
+        for i in range(20):
+            p = multiprocessing.Process(target=Banco.retirar, args=(cuenta, 50))
+            procesos.append(p)
+            p.start()
+        for i in range(60):
+            p = multiprocessing.Process(target=Banco.retirar, args=(cuenta, 20))
+            procesos.append(p)
+            p.start()
+        for p in procesos:
+            p.join()
+        print(f'El saldo de la cuenta final es de {cuenta.value} €')
+
+if __name__ == '__main__':
+    Banco.main()
+
